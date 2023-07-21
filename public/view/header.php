0e36200ff2d42168dccf33a1e9aed76c86b96b8f
GitNaf <114709801+NafMn@users.noreply.github.com>
2023-07-21 18:56:27 +0700

    update

--- public/view/header.php
+++ public/view/header.php
@@ -1,15 +1,26 @@
-    <header>
-        <div class="logo">
-            <img src="../assets/images/iconprofile.png" alt="Logo Perusahaan">
+<!-- Navigasi -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Cek Kesehatan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tentang</a>
+          </li>
+        </ul>
+        <!-- Tombol Login -->
+        <div class="col-auto">
+            <button type="button" class="btn btn-primary">Login</button>
         </div>
-        <nav>
-            <ul>
-                <li><a href="#">Beranda</a></li>
-                <li><a href="#">Cek Kesehatan</a></li>
-                <li><a href="#">Tentang</a></li>
-            </ul>
-        </nav>
-        <div class="login-btn">
-            <a href="#">Login</a>
-        </div>
-    </header>
+      </div>
+    </div>
+</nav>
